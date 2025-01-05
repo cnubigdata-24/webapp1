91f38557bbfddb1acb38f56b84da9e0d64a1bbed
cnubigdata <cnubigdata@gmail.com>
2025-01-05 16:49:59 +0900

    Fix Flake8 errors in test code

--- app/main.py
+++ app/main.py
@@ -1,6 +1,5 @@
 from flask import Flask
-from datetime import datetime
-
+from datetime import datetime  # datetime 모듈 추가
 
 app = Flask(__name__)
 
@@ -10,3 +9,6 @@ def home():
     current_time = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
     return f"Hello, World! CI/CD Test with GitHub Action and MS Azure !!!\nCurrent Time: {current_time}"
 
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=80)  # 로컬 실행 추가
